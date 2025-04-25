ac8ec6f5bcd49b9765c152be7c47e138153fcf76
nishntraut17 <nishntraut17@gmail.com>
2025-04-25 21:28:01 +0530

    modified user controller and application.properties

--- src/main/java/com/practice/user_repo/controller/UserRepoController.java
+++ src/main/java/com/practice/user_repo/controller/UserRepoController.java
@@ -1,6 +1,7 @@
 package com.practice.user_repo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +12,19 @@ import java.util.List;
 @RestController
 @RequestMapping
 public class UserRepoController {
+    static private List<String> users = null;
 
+    UserRepoController(){
+        users = new ArrayList<String>(Arrays.asList("Nishant","Aditya","Rajesh"));
+    }
+
+    @GetMapping("/users")
     public ResponseEntity<Object> getUsers(){
-        List<String> users = new ArrayList<String>(Arrays.asList("Nishant","Aditya","Rajesh"));
         return ResponseEntity.ok(users);
+    }
+
+    public ResponseEntity<Object> saveUser(String user){
+        users.add(user);
+        return ResponseEntity.ok("User added successfully");
     }
 }
